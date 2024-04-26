79c19dd1db3c924566b048f59c5f656de3bdcabf
vprint <valentin.printemps@gmail.com>
2024-04-26 19:45:48 +0200

    [MapSelector] Ajout du selecteur
    Ajout du sélecteur sur la carte + mise à jour des paramètres.

---
+++ src/services/MapSelector.ts
@@ -0,0 +1,86 @@
+import { Platform } from 'quasar';
+import { Map } from 'ol';
+import BaseLayer from 'ol/layer/Base';
+import VectorTileLayer from 'ol/layer/VectorTile';
+import { useSiteStore } from 'src/stores/site-store';
+import { Pixel } from 'ol/pixel';
+
+const siteStore = useSiteStore();
+
+/**
+ * Create a selector over the site site layer
+ */
+export class MapSelector {
+  private map: Map;
+  private siteLayer: VectorTileLayer;
+
+  constructor(map: Map, platform: Platform) {
+    this.map = map;
+    this.siteLayer = this.map
+      .getAllLayers()
+      .find(
+        (layer: BaseLayer) => layer.get('name') === 'sites'
+      ) as VectorTileLayer;
+
+    if (platform.is.mobile) {
+      this.mobileSelection();
+    }
+    if (platform.is.desktop) {
+      this.desktopSelection();
+    }
+  }
+
+  /**
+   * Initiate mobile selector
+   */
+  private async mobileSelection(): Promise<void> {
+    this.map.getTargetElement().oncontextmenu = async (
+      e: MouseEvent
+    ): Promise<void> => {
+      e.preventDefault();
+      const pixel = this.map.getEventPixel(e);
+      const featureId = await this.getSiteFeatureId(pixel);
+
+      if (featureId) {
+        siteStore.fetchAndSetSite(featureId);
+      }
+    };
+  }
+
+  /**
+   * Initiate desktop selector
+   */
+  private async desktopSelection(): Promise<void> {
+    this.map.on('click', async (e) => {
+      const featureId = await this.getSiteFeatureId(e.pixel);
+
+      if (featureId) {
+        siteStore.fetchAndSetSite(featureId);
+      }
+    });
+  }
+
+  /**
+   * Get site feature id at pixel
+   * @param e Event
+   * @returns Feature id
+   */
+  private getSiteFeatureId(pixel: Pixel): Promise<number | undefined> {
+    const feature = this.siteLayer
+      .getFeatures(pixel)
+      .then(function (features): number | undefined {
+        if (!features.length) {
+          return;
+        }
+
+        const feature = features[0] ? features[0] : undefined;
+
+        if (!feature) {
+          return;
+        }
+
+        return feature.get('site_id');
+      });
+    return feature;
+  }
+}
--- src/utils/params/app.ts
+++ src/utils/params/app.ts
@@ -1,6 +1,6 @@
 export enum APP_SETTINGS {
   applicationName = 'Archaeo-Web',
-  qgisServer = 'http://localhost:8081/cgi-bin/qgis_mapserv.fcgi.exe?',
-  vectorTileServer = 'http://localhost:6767',
-  featureServer = 'http://localhost:9010/FeatureServer',
+  qgisServer = 'http://192.168.1.13:8081/cgi-bin/qgis_mapserv.fcgi.exe?',
+  vectorTileServer = 'http://192.168.1.13:6767',
+  featureServer = 'http://192.168.1.13:9010/FeatureServer',
 }
