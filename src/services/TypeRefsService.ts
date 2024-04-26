60b6ebb8294020c7b5978aa60f0d05c5a358c7c5
vprint <valentin.printemps@gmail.com>
2024-04-27 01:10:05 +0200

    [TypeRef] Suppression de typeRefService

--- src/services/TypeRefsService.ts
+++
@@ -1,6 +0,0 @@
-import { SITE_TYPE_REFS_PARAMS } from '../utils/params/typeRefsSettings';
-
-export function getTypeRef(value: string): string {
-  const typeRef = SITE_TYPE_REFS_PARAMS[value];
-  return typeRef;
-}
