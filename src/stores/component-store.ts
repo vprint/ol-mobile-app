d95b0443e3713fb352d0b8f81566c94a635a4e7b
vprint <valentin.printemps@gmail.com>
2024-05-12 12:58:01 +0200

    maj store

--- src/stores/component-store.ts
+++ src/stores/component-store.ts
@@ -1,22 +1,40 @@
 import { defineStore } from 'pinia';
 import { IWidgetName, Widget } from 'src/model/widget';
+import { computed, ref } from 'vue';
 
-export const useComponentStore = defineStore('component', {
-  state: () => ({
-    _widget: new Widget(),
-  }),
-  getters: {
-    widget: (state) => state._widget,
-  },
-  actions: {
-    /**
-     * Menage widget visibility
-     * @param visibility Widget visibility
-     * @param widgetName Name of the widget to display
-     */
-    setWidget(visibility: boolean, widgetName: IWidgetName): void {
-      this._widget.visibility = visibility;
-      this._widget.widgetName = widgetName;
-    },
-  },
+// export const useComponentStore = defineStore('component', {
+//   state: () => ({
+//     _widget: new Widget(),
+//   }),
+//   getters: {
+//     widget: (state) => state._widget,
+//   },
+//   actions: {
+//     /**
+//      * Menage widget visibility
+//      * @param visibility Widget visibility
+//      * @param widgetName Name of the widget to display
+//      */
+//     setWidget(visibility: boolean, widgetName: IWidgetName): void {
+//       this._widget.visibility = visibility;
+//       this._widget.widgetName = widgetName;
+//     },
+//   },
+// });
+
+export const useComponentStore = defineStore('component', () => {
+  const _widget = ref(new Widget());
+  const widget = computed(() => _widget.value);
+
+  /**
+   * Menage widget visibility
+   * @param visibility Widget visibility
+   * @param widgetName Name of the widget to display
+   */
+  function setWidget(visibility: boolean, widgetName: IWidgetName): void {
+    _widget.value.visibility = visibility;
+    _widget.value.widgetName = widgetName;
+  }
+
+  return { widget, setWidget };
 });
